53ff1ae423d264efcf5bdd9e0ea981b122f886af
Rafa Ferrera <rafafena@gmail.com>
2024-10-29 22:00:48 +0100

    Change in minify and uglify for production

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-    build: {
-        minify: true
+  build: {
+    minify: 'terser', // Indica a Vite que use Terser
+    terserOptions: {
+      compress: {
+        drop_console: true, // Opcional: elimina los console.log en producción
+      },
+      format: {
+        comments: false, // Elimina los comentarios
+      },
     }
-});
+  }
+})
